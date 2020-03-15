f6432e21191ccb396fd38596374a5ea73dbe3681
Vincent Schoener <vincent.schoener@gmail.com>
2020-03-15 16:39:42 +0100

    Fix background on larger screen

--- src/components/subscribe/subscribe-style.tsx
+++ src/components/subscribe/subscribe-style.tsx
@@ -3,4 +3,5 @@ import styled from 'styled-components';
 export const MainSection = styled.section`
   background-image: url('/img/bg-img/main.jpg');
   background-repeat: none;
+  background-size: cover;
 `;
